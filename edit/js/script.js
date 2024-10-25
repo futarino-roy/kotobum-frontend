0b2b0a243fb22c43b0ff68d706d79f36f9cc2ef8
Yuki Nabeshima <futarino.m4@gmail.com>
2024-10-25 14:11:29 +0900

    cssUrls以外のJSON形式を変更しました

--- edit/js/script.js
+++ edit/js/script.js
@@ -1050,15 +1050,15 @@ document.getElementById('sendButton').addEventListener('click', function () {
         const body = new FormData();
         body.append('htmlContent', htmlContent);
         body.append('cssContent', cssContent);
-        // body.append('cssUrls', JSON.stringify(cssUrls));
-        // body.append('localStorageData', JSON.stringify(localStorageData));
-        // body.append('newImageDatabase1Data', JSON.stringify(newImageDatabase1Data));
-        // body.append('imageDBData', JSON.stringify(imageDBData));
+        body.append('cssUrls', JSON.stringify(cssUrls));
+        body.append('localStorageData', JSON.stringify(localStorageData));
+        body.append('newImageDatabase1Data', JSON.stringify(newImageDatabase1Data));
+        body.append('imageDBData', JSON.stringify(imageDBData));
 
-        localStorageData.forEach(url => body.append('localStorageData[]', url));
-        newImageDatabase1Data.forEach(url => body.append('newImageDatabase1Data[]', url));
-        imageDBData.forEach(url => body.append('imageDBData[]', url));
-        cssUrls.forEach(url => body.append('cssUrls[]', url));
+        // localStorageData.forEach(url => body.append('localStorageData[]', url));
+        // newImageDatabase1Data.forEach(url => body.append('newImageDatabase1Data[]', url));
+        // imageDBData.forEach(url => body.append('imageDBData[]', url));
+        // cssUrls.forEach(url => body.append('cssUrls[]', url));
 
         // 送信するデータをコンソールに出力
         console.log('送信するデータ:', {
