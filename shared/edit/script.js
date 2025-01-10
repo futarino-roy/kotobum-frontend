b1e6b50765f99bb6b9ed8a9f26c76a535ffb75a4
Yuki Nabeshima <futarino.m4@gmail.com>
2025-01-10 15:28:13 +0900

    JS修正　rawData修正7

--- shared/edit/script.js
+++ shared/edit/script.js
@@ -528,7 +528,6 @@ function getCroppieResult() {
   if (window.croppieInstance) {
     window.croppieInstance.result({ type: 'raw', size: 'original' })
       .then((rawData) => {
-        console.log('トリミング情報:', rawData);
         const { points, zoom, origin } = rawData;
         console.log('トリミング情報:');
         console.log('開始X座標:', points[0]);
@@ -537,6 +536,7 @@ function getCroppieResult() {
         console.log('終了Y座標:', points[3]);
         console.log('ズームレベル:', zoom);
         console.log('元画像URL:', origin);
+        console.log('トリミング情報:', rawData);
       }).catch((error) => {
         console.error('トリミング情報取得失敗:', error);
       });
